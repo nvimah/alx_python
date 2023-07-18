dd1961773165436e87359f9148e0960a5e5a7e1a
nvimah <101018019+nvimah@users.noreply.github.com>
2023-07-18 10:35:37 +0300

    my third python file

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,10 @@
+import random
+number = random.randint(-10000, 10000)
+# YOUR CODE HERE
+print("last digit of",{},"is",{}, .format(number,number=number[-1])) 
+if lastdigit > 5:
+    print("and is greater than 5")
+elif lastdigit == 0:
+    print("and is 0")
+elif lastdigit < 6 > 0 :
+    print("and is less than 6 and not 0")
