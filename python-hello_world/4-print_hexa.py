a30cb76873c6e760414448621f730f8dd074b2dc
nvimah <101018019+nvimah@users.noreply.github.com>
2023-07-19 09:25:49 +0300

    one print function deleted

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -2,8 +2,7 @@
 number= range(0,99)
 #loop around the numbers
 for i in range(0,99):
-    print(i, end="")
     hex_value= "{0:x}".format(i)
 #convert the numbers to hexadecimal
-    print(" = 0*{}" .format(hex_value))
+    print(i,"= 0*{}" .format(hex_value) )
 #print with a string format 
