fb8e77733af6c98bb47f78ae2409763f33f3f56e
nvimah <101018019+nvimah@users.noreply.github.com>
2023-07-19 17:58:36 +0300

    print function on line four removed

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,12 +1,11 @@
 import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
-print(number, end="")
 if number > 0:
     print("{}: is positive" .format(number))
 elif number==0:
     print("{}: is zero" .format(number))
-else:
+else: 
     print("{} is negative" .format(number))
     
     
