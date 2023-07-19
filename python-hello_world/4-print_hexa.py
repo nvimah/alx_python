d2c417f0ff2b2816f9de8dd4c3bda7c9aaf67949
nvimah <101018019+nvimah@users.noreply.github.com>
2023-07-19 09:20:44 +0300

    task four done

---
+++ python-hello_world/4-print_hexa.py
@@ -0,0 +1,9 @@
+# get the range of numbers from 0 to 98
+number= range(0,99)
+#loop around the numbers
+for i in range(0,99):
+    print(i, end="")
+    hex_value= "{0:x}".format(i)
+#convert the numbers to hexadecimal
+    print(" = 0*{}" .format(hex_value))
+#print with a string format 
