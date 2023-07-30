d37ed08ea113df07c142362f3bb9bc2bec718827
nvimah <101018019+nvimah@users.noreply.github.com>
2023-07-30 17:53:15 +0300

    variable imported

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,15 @@
+# print the number of arguments
+arguments = ["Hello", "Holberton", "School", "98", "Battery", "street"]
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print the list of arguments
+no_arguments = numbers
+if no_arguments == 0 :
+    print(f"{no_arguments} arguments.")
+elif no_arguments > 0:
+    print(f"{no_arguments} arguments :")
+else:
+    pass
+
+
+
+
---
+++ python-import_modules/2-variable_load.py
@@ -0,0 +1,2 @@
+import variable_load_2
+print(variable_load_2.a)
