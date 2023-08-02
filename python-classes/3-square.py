a666e43354ffe2f7286699e823caa2b674539612
nvimah <101018019+nvimah@users.noreply.github.com>
2023-08-02 11:48:06 +0300

    if replicated to line 10-15

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,6 +7,12 @@ class Square:
         def size(self):
             return self.__size
         '''size is allocated a private attribute'''
+        if not isinstance (size, int):
+            raise TypeError ("size must be an integer")
+        '''typeerror is raised if the size is not an integer'''
+        if size < 0:
+            raise ValueError ("size must ne >= 0")
+        '''value error is raised if a negetive number or zero is input'''
         def size(self, value):
          '''size is cahanged to value'''
          if not isinstance (value, int):
