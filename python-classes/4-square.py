73dc42dd47f127c7b4131a395afc8828ea1f7514
nvimah <101018019+nvimah@users.noreply.github.com>
2023-08-02 12:18:04 +0300

    comments added

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,13 +1,14 @@
-'''this is a square class with a size attribute andseveral instances '''
+'''this is a square class with a size attribute and several instances or objects of the square class '''
 class Square:
-    '''this is he first class named square'''
+    '''this is the first class named square that contains size attributes'''
     def __init__(self, size=0):
         self.size = size
-        '''the size attribute is set to size'''
+        '''the size attribute is set to size as defined above before converting t to a private attribute'''
     def size (self):
         return self.__size
-    '''the size attribute is changed to a private attribute'''
+    '''the size attribute is changed to a private attribute by adding the double underscore'''
     def size(self, value):
+      '''the size attribute is the changed to a value'''
       if not isinstance (value, int):
              raise TypeError ('size must be an integer')
       '''if the value for the size is not an integer raise a typeerror'''
@@ -25,6 +26,6 @@ class Square:
             for _ in range (self.__size):
                  print ("#" * self.__size)
                  '''else print a hashtag for every value keyed in'''
-
+'''the class square and its atribute sizes and all of its instances are excecured'''
          
     
