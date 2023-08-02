779b30cba608c0decb2ad9786d2659970c02e577
nvimah <101018019+nvimah@users.noreply.github.com>
2023-08-02 09:33:47 +0300

    private attribute updated

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 class Square:
-    def __init__(self, _size):
-        self._size = _size
+    def __init__(self, size):
+        self.__size = size
 my_square = Square(3)
 print(type(my_square))
 print(my_square.__dict__)
