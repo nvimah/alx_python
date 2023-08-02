954d364bbb3aee993297e710ac512038a016a2d3
nvimah <101018019+nvimah@users.noreply.github.com>
2023-08-02 10:08:39 +0300

    code documentation added

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,16 @@
 class Square:
+    '''
+    This is a class that contains a square and will be use as a refernce to all the class instances
+    '''
     def __init__(self, size):
+        '''
+        The init is used to allocate attributes to our class
+        the first parameter references to the class
+        the second parameter is the size attribute
+        '''
         self.__size = size
-         #  Initializes the square with a given size.
-          # Parameters:
-            # size (int): The size of the square's side.
+        ''' 
+         Initializes the square with a given size.
+         size (int): The size of the square's side
+         '''
 my_square = Square
