b7427d1cf7e72e80d45f3d8d939e4659a0ff461d
nvimah <101018019+nvimah@users.noreply.github.com>
2023-08-04 06:11:47 +0300

    task 4

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,5 @@
+'''This class has no attributes or methods defined, as it is just an empty class definition.'''
+class BaseGeometry:
+    '''You can later use this class as a base class to create other classes that will inherit from it and add attributes and methods as needed.'''
+    pass
+'''this is used for pep-8'''
