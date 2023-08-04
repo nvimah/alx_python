2fee29fd1ca66faa918b9242835d90bf040a1817
nvimah <101018019+nvimah@users.noreply.github.com>
2023-08-04 06:21:16 +0300

    task five

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,12 @@
+'''This class contains a method area that raises an exception'''
+class BaseGeometry:
+    '''In this class, we define the area() method, and when this method is called, it raises an Exception with the specified message "area() is not implemented". '''
+    def area(self):
+     '''In this class, we define the area() method, and when this method is called, it raises an Exception with the specified message "area() is not implemented".'''
+    raise Exception("area() is not implemented")
+    '''on an instance of the derived class will raise the exception indicating that the method needs to be implemented in the subclass.
+
+
+
+
+'''
